816ec9e6ac06b55fa4da2964b0decae394d44d21
gantlone <edison0963125746@gmail.com>
2023-10-21 15:56:16 +0800

    2-1 學習基本dart參數應用 int string

---
+++ practice_again_20231021/lib/2-1.dart
@@ -0,0 +1,14 @@
+void main() {
+  String jobInfo = '全端工程師';
+
+  String description = '會寫給用戶看的，也會寫接觸資料庫的';
+
+  int salary = 35000;
+
+  print(jobInfo + "\n" + description + '\n' + salary.toString());
+
+  String feature1 = '大話AWS雲端架構';
+  int feature2 = 140;
+
+  print(feature1 + '\n' + '重量是:' + feature2.toString());
+}
